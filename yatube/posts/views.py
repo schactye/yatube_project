b12f0efa58bbf9ed408e9fc8d43cb65ca4982016
“Елена <borovina@bk.ru>
2021-12-04 13:51:03 +0300

    изменtybz

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse(‘Главная страница‘)
+    return HttpResponse('Главная страница')
 
-def group_posts(request):
-    return HttpResponse(f’блог‘{pk}’)
+def group_posts(request, pk):
+    return HttpResponse(f'Блог {pk}')
