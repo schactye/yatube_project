2079644fd183d925cbbac99815a78d69cdb65a19
“Елена <borovina@bk.ru>
2021-12-09 15:33:16 +0300

    9 декабря

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
 
 def index(request):
-    return HttpResponse('Главная страница')
+    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, 'posts/index.html', context)
 
-def group_posts(request, pk):
-    return HttpResponse(f'Блог {pk}')
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    title = f'Записи сообщества {group}'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
